e1ffc77d2147d63936d05fb3d293f970fe33b1a2
DOHA <koo9811@gmail.com>
2024-03-04 14:32:21 +0900

    .

--- Brute_force/3085번(사탕_게임).py
+++ Brute_force/3085번(사탕_게임).py
@@ -2,3 +2,16 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+Candybox = [0 for _ in range(N)]
+
+for i in range(0,N):
+    Candybox[i] = list(map(str,input().strip()))
+
+Maxright = 0
+Maxup = 0
+Max = 0
+
+def checkMax():
+    for i in range(0,N):
+        for j in range(0,N):
+            

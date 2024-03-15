8d10c760b63d909f38fc9565e960b02d44d8805c
DOHA <koo9811@gmail.com>
2024-03-15 16:44:55 +0900

    .

--- C_Study_With_Algorism/pointer_exercise_01.c
+++ C_Study_With_Algorism/pointer_exercise_01.c
@@ -2,7 +2,7 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-void ChangeDouble(int* p) { /*int*p=&num;*/
+void ChangeTriple(int* p) { /*int*p=&num;*/
     *p = *p * 3; 
 }
 int main()
@@ -10,7 +10,7 @@ int main()
     int num;
     printf("숫자를 입력하시오 : ");
     scanf("%d", &num); /*&num은 num의 변수 주소*/
-    ChangeDouble(&num);
+    ChangeTriple(&num);
     printf("%d\n", num);
     return 0;
 }

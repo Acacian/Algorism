430b40156cb781f33e85e893c4a0eafb1227fc37
DOHA <koo9811@gmail.com>
2024-03-15 16:18:39 +0900

    .

--- C_Study_With_Algorism/pointer_exercise_01.c
+++ C_Study_With_Algorism/pointer_exercise_01.c
@@ -1,3 +1,5 @@
+//int형 변수를 선언하여 사용자에게 입력받은 후 해당 값을 2배로 바꿔주는 함수(반환 형태 : void)를 사용하여 입력받은 수의 3배를 출력하시오
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 void ChangeDouble(int* p) { /*int*p=&num;*/
@@ -7,7 +9,7 @@ int main()
 {
     int num;
     printf("숫자를 입력하시오 : ");
-    scanf_s("%d", &num); /*&num은 num의 변수 주소*/
+    scanf("%d", &num); /*&num은 num의 변수 주소*/
     ChangeDouble(&num);
     printf("%d\n", num);
     return 0;

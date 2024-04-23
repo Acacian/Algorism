6c64c6e44358c97ebab8bce536b4c61c78d6d90b
DOHA <koo9811@gmail.com>
2024-04-23 11:06:24 +0900

    up

--- Basic/1427.py
+++
@@ -1,7 +0,0 @@
-import queue from collections
-import sys
-
-input = sys.stdin.readline()
-
-ls = map(int, input.split())
-
---
+++ unsolved/1427.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+ls = str(input().strip)
+
+# sorted(ls)
+
+# while (ls // 10 == 0):
+#     lsl.append(ls % 10)
+#     ls = ls // 10
+#     print(ls)
+
+# lsq = deque(ls)
+# print(lsq)
+
+# for i in range(N
